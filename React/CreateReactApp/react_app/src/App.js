83ad731778d38ee94b3cbc2f795edf03b2f763db
Mahmoud Anbousi <anbousi@gmail.com>
2021-07-13 13:29:10 +0300

    First JSX core

---
+++ React/CreateReactApp/react_app/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo</h1>
+      <h3>Things I need to do:</h3>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/CreateReactApp/react_app/src/App2.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function M() {
+  return (
+    <div className="App">
+      <ul>
+        <li>Learn React</li>
+        <li>Climp Mt. Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+      </ul>
+    </div>
+  );
+}
+
+export default M;
