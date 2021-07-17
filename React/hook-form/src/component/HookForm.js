67e1c12d05047259f6047a6af7a5ea0abded4eb6
Mahmoud Anbousi <anbousi@gmail.com>
2021-07-17 12:22:59 +0300

    More Forms

--- React/hook-form/src/component/HookForm.js
+++ React/hook-form/src/component/HookForm.js
@@ -2,10 +2,15 @@ import React , {useState} from 'react'
 
 const HookForm = (props) => {
     const [firstName, setFirstName] = useState("");
+    const [errorFN , seterrorFN] = useState("");
     const [lastName, setLastName] = useState("");
+    const [errorLN , seterrorLN] = useState("");
     const [email, setEmail] = useState("");
+    const [errorEmail , seterrorEmail] = useState("");
     const [password, setPassword] = useState("");  
-    const [confirmPW, setconfirmPW] = useState("");  
+    const [errorPW , seterrorPW] = useState("");
+    const [confirmPW, setconfirmPW] = useState("");
+    const [errorPWC , seterrorPWC] = useState("");  
     
     const createUser = (e) => {
         e.preventDefault();
@@ -18,6 +23,54 @@ const HookForm = (props) => {
         setconfirmPW("");
     };
 
+    const validationFN = (e) =>{
+        setFirstName(e.target.value);
+        if(e.target.value.length === 0){
+            seterrorFN("Enter first name");
+        }
+        else if (e.target.value.length <= 2){
+            seterrorFN("Fisrt Name should be more than 2 chars");
+        }
+        else{
+            seterrorFN("");
+        }
+    }
+
+    const validationLN = (e) =>{
+        setLastName(e.target.value);
+        if(e.target.value.length === 0){
+            seterrorLN("Enter last name");
+        }
+        else if (e.target.value.length <= 2){
+            seterrorLN("Last Name should be more than 2 chars");
+        }
+        else{
+            seterrorLN("");
+        }
+    }
+
+    const validationPW = (e) =>{
+        setPassword(e.target.value);
+
+    if (e.target.value.length < 8){
+        seterrorPW("Password should be 8 chars at least");
+        }
+    else{
+        seterrorPW("");
+    }
+    }
+
+    
+    const validationPWC = (e) =>{
+        setconfirmPW(e.target.value);
+
+    if (e.target.value === {password}){
+        seterrorPWC("");
+        }
+    else{
+        seterrorPWC("Password should match");
+    }
+    }
 
     
     return(
@@ -26,27 +79,33 @@ const HookForm = (props) => {
                 <table>
                     <tr>
                         <td><label>First Name: </label></td>
-                        <td><input type="text" onChange={ (e) => setFirstName(e.target.value) } value={firstName} /></td>
+                        <td>
+                            <input type="text" onChange={ validationFN } value={firstName} />
+                        </td>
+                        <td>{errorFN}</td>
                     </tr>
 
                     <tr>
                         <td><label>Last Name: </label></td>
-                        <td><input type="text" onChange={ (e) => setLastName(e.target.value) } value={lastName} /></td>
+                        <td><input type="text" onChange={ validationLN } value={lastName} /></td>
+                        <td>{errorLN}</td>
                     </tr>
 
                     <tr>
                         <td><label>Email Address: </label> </td>
-                        <td><input type="text" onChange={ (e) => setEmail(e.target.value) } value={email}/></td>
+                        <td><input type="email" onChange={ (e) => setEmail(e.target.value) } value={email}/></td>
                     </tr>
 
                     <tr>
                         <td><label>Password: </label></td>
-                        <td><input type="password" onChange={ (e) => setPassword(e.target.value) } value={password} /></td>
+                        <td><input type="password" onChange={ validationPW } value={password} /></td>
+                        <td>{errorPW}</td>
                     </tr>
 
                     <tr>
                         <td><label>Confirm Password: </label></td>
-                        <td> <input type="password" onChange={ (e) => setconfirmPW(e.target.value) } value={confirmPW} /></td>
+                        <td> <input type="password" onChange={ validationPWC } value={confirmPW} /></td>
+                        <td>{errorPWC}</td>
                     </tr>
                 </table>
                 <input type="submit" value="Create User" />
