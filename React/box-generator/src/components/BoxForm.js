c873bce66e5690cfd8344a9391a3196570039098
Mahmoud Anbousi <anbousi@gmail.com>
2021-07-18 16:41:44 +0300

    validation added

--- React/box-generator/src/components/BoxForm.js
+++ React/box-generator/src/components/BoxForm.js
@@ -3,19 +3,38 @@ import React ,{useState} from 'react'
 const BoxForm = (props) => {
     const [ color , setColor] = useState("");
     const [ size , setSize] = useState("");
+    const [error , setError] = useState("")
 
     const selectColor = (e) => {
         setColor(e.target.value);
+        if(e.target.value<1 ){
+            setError("Enter Value")
+        }
+        else{
+            setError("")
+        }
     }
 
     const selectSize = (e) => {
         setSize(e.target.value);
+        if(e.target.value<1 ){
+            setError("Enter Value")
+        }
+        else{
+            setError("")
+        }
     }
 
     const BoxColor = (e)=>{
         e.preventDefault();
-        console.log({color}, {size});
-        props.onNewColor(color , size);
+        if(color.length<1 || size.length<1){
+            setError("Enter Value")
+        }
+        else{
+            console.log({color}, {size});
+            props.onNewColor(color , size);
+            setError("")
+        }
     }
 
     return (
@@ -23,6 +42,7 @@ const BoxForm = (props) => {
             <form onSubmit = {BoxColor}>
                 <input onChange={selectColor} type="text" value={color}></input>
                 <input onChange={selectSize} type="number" value={size}></input>
+                {error}
                 <button type="submit">Add</button>
             </form>
         </div>
