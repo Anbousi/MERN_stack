e1a2633d92251699e34a4771c05bc6acf58078bb
Mahmoud Anbousi <anbousi@gmail.com>
2021-07-19 15:52:53 +0300

    tabs

--- React/tabs/src/components/Tab.js
+++ React/tabs/src/components/Tab.js
@@ -4,15 +4,16 @@ import style from './style.module.css'
 
 const Tab = () => {
     const [text , setText] = useState("")
-    const[Tab0,setTab0]=useState({"text":"this is tab1" , "backColor":"red"})
-    const[Tab1,setTab1]=useState({"text":"this is tab2" , "backColor":"orange"})
-    const[Tab2,setTab2]=useState({"text":"this is tab3" , "backColor":"green"})
+    const[Tab0,setTab0]=useState({"text":"this is tab1" , "backColor":"white"})
+    const[Tab1,setTab1]=useState({"text":"this is tab2" , "backColor":"white"})
+    const[Tab2,setTab2]=useState({"text":"this is tab3" , "backColor":"white"})
     // const Tab0 = {"text":"this is tab1" , "backColor":"red"} 
     // const Tab1 = {"text":"this is tab2" , "backColor":"orange"}
     // const Tab2 = {"text":"this is tab3" , "backColor":"green"}
     const arr = [Tab0 , Tab1 , Tab2];
     const stylo =(e,tab)=>{
         setText(tab.text);
+        arr.filter(t => t.backColor === 'black').map(t => t.backColor = 'white');
         tab.backColor="black";
     }
     return (
