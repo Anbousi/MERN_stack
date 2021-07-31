a4a3d05e7cf34a62c58c3d4044a1ef08689ac729
Mahmoud Anbousi <anbousi@gmail.com>
2021-07-31 14:41:20 +0300

    Jokes API

--- MongoDB/JokesApi/server/models/joke.model.js
+++ MongoDB/JokesApi/server/models/joke.model.js
@@ -1,8 +1,17 @@
 const mongoose = require("mongoose");
 
 const JokeSchema = new mongoose.Schema({
-	setup: String,
-	punchline: String
+	setup: {
+		type: String,
+		required: [true, 'This field is required'],
+		minlength:[10, 'Must be at least 10 chars'],
+	},
+
+	punchline:{
+		type : String,
+		required: [true, 'This field is required'],
+		minlength:[3, 'Must be at least 3 chars'],
+	} 
 });
 
 const Joke = mongoose.model("Joke", JokeSchema);
